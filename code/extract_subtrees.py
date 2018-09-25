f0c6e37a4a80e130cfc03198bbe65d7af6ddcc19
Evelin Ács <acs.evelin@gmail.com>
2018-09-25 20:57:06 +0200

    Add subtree extraction script

---
+++ code/extract_subtrees.py
@@ -0,0 +1,50 @@
+import sys
+import argparse
+
+from nltk.corpus import treebank
+
+from utils import REPLACE_MAP
+from utils import sanitize_word
+
+parser = argparse.ArgumentParser(description = "Extract subtrees from Penn Treebank")
+parser.add_argument("phrase_level", type = str, help = "the name of the phrase level to extract, e.g. NP, ADJP")
+parser.add_argument("-s", "--sanitize", action = "store_true", help = "sanitize the output")
+
+args = parser.parse_args()
+
+
+def sanitize_label(tree): #sanitize labels which contain hyphens (e.g.NP-SBJ)
+    tree.set_label(tree.label().replace("-", "_"))
+
+def sanitize_pos(tree): #replace punctuation pos-tags
+    tree_label = tree.label()
+    is_punct = True
+    for character in tree_label:
+        if character not in REPLACE_MAP:
+            is_punct = False
+    if is_punct == True:
+        tree.set_label("PUNCT")
+
+def sanitize_tree(tree):
+    sanitize_label(tree)
+    if tree.height() == 2: #word, pos
+        sanitize_pos(tree)
+        tree[0] = sanitize_word(tree[0]) #tree[0] == word
+    else:
+        for subtree in tree:
+            sanitize_tree(subtree)
+
+
+def main(phrase_level, sanitize):
+    for n in range(1,200):
+        tree_file = "wsj_{}.mrg".format(str(n).zfill(4))
+        sentences = treebank.parsed_sents(tree_file)
+        for s in sentences:
+            for subtree in s.subtrees(lambda t: t.label() == phrase_level):
+                if sanitize == True:
+                    sanitize_tree(subtree)
+                print(subtree.pformat(100000))
+                break
+
+if __name__ == "__main__":
+    main(args.phrase_level, args.sanitize)
---
+++ code/utils.py
@@ -0,0 +1,50 @@
+"""
+python3 version of 
+https://github.com/kornai/4lang/blob/master/exp/alto/tools/common.py
+"""
+
+REPLACE_MAP = {
+    ":": "COLON",
+    ",": "COMMA",
+    ".": "PERIOD",
+    ";": "SEMICOLON",
+    "-": "HYPHEN",
+    "[": "LSB",
+    "]": "RSB",
+    "(": "LRB",
+    ")": "RRB",
+    "{": "LCB",
+    "}": "RCB",
+    "!": "EXC",
+    "?": "QUE",
+    "'": "SQ",
+    '"': "DQ",
+    "/": "PER",
+    "\\": "BSL",
+    "#": "HASHTAG",
+    "%": "PERCENT",
+    "&": "ET",
+    "@": "AT",
+    "$": "DOLLAR",
+    "*": "ASTERISK",
+    "^": "CAP",
+    "`": "IQ",
+    "+": "PLUS",
+    "|": "PIPE",
+    "~": "TILDE",
+    "<": "LESS",
+    ">": "MORE",
+    "=": "EQ"
+}
+
+KEYWORDS = set(["feature"])
+
+
+def sanitize_word(word):
+    for pattern, target in REPLACE_MAP.items():
+        word = word.replace(pattern, target)
+    for digit in "0123456789":
+        word = word.replace(digit, "DIGIT")
+    if word in KEYWORDS:
+        word = word.upper()
+    return word
