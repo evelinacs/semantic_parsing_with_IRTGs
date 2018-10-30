c5ee980433063df4c92ad63860715547d3f04832
Evelin Ács <acs.evelin@gmail.com>
2018-10-30 14:08:11 +0100

    Add generating scripts for unary VPs and PPs

---
+++ code/generate_grammar/hollo1996/firstTry/basicUnaryPP.py
@@ -0,0 +1,32 @@
+from Data import *
+"""
+        //PrepositionalPhrase out of one Word
+        //PP(WD(stuff))
+        PP -> one_WD(WD)
+        [tree] VP(?1)
+        [ud] ?1
+        [fourlang] ?1
+"""
+
+# unary PPs
+
+def basicUnaryPP():
+    result=""
+
+    datas=Data.fromFile("data/Input/Unary/unary_pp_trees", "data/Input/Unary/unary_pp_terms", "data/Input/Unary/unary_pp_deps", 3)
+    uniqData={}
+    for i in range(len(datas)-1,-1,-1):
+        if not (uniqData.keys().__contains__(datas[i].tree.wordLess)):
+            uniqData[datas[i].tree.wordLess]=datas[i]
+
+    for input in uniqData.values():
+        result+="\n"
+        result+="//PrepositionalPhrase out of one " + input.words[0].type.Longer + ".\n"
+        result+="//" + input.tree.stanford+"\n"
+        result+="//" + input.tree.tagTree+"\n"
+        result+="PP -> one_" + input.words[0].type.Shortest.replace("basic", "") + "(" + input.words[0].type.Shortest + ")\n"
+        result+="[string] ?1\n"
+        result+="[tree] PP(?1)\n"
+        result+="[ud] ?1\n"
+        result+="[fourlang] ?1\n"
+    return result
