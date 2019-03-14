a3bc3c78e3cc233dc119b6911dbeee56af6a3bc6
Evelin Ács <acs.evelin@gmail.com>
2019-03-14 10:46:16 +0100

    Update text_from_trees.py

--- code/generate_grammar/template_based_grammar_generator/eval/text_from_trees.py
+++ code/generate_grammar/template_based_grammar_generator/eval/text_from_trees.py
@@ -1,5 +1,24 @@
 import sys
+
+from utils import REPLACE_MAP
+from utils import sanitize_word
+
 from nltk.tree import Tree
+
+
+def sanitize_label(tree): #sanitize labels which contain hyphens (e.g.NP-SBJ)
+    for key in REPLACE_MAP:
+        tree.set_label(tree.label().replace(key, REPLACE_MAP[key]))
+
+
+def sanitize_tree(tree):
+    sanitize_label(tree)
+    if tree.height() == 2: #word, pos
+        tree[0] = sanitize_word(tree[0]) #tree[0] == word
+    else:
+        for subtree in tree:
+            sanitize_tree(subtree)
+
 
 def get_text_from_trees(fn):
     """
@@ -9,9 +28,12 @@ def get_text_from_trees(fn):
     with open(fn) as tree_file:
         for line in tree_file:
             tree = Tree.fromstring(line)
+            sanitize_tree(tree)
+
             words = tree.leaves()
             np_list.append(words)
     for sublist in np_list:
         print(*sublist[:-1]) # to remove the dot
+            # print(tree.pformat(10000000))
 
 get_text_from_trees(sys.argv[1])
